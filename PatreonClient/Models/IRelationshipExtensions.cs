3ed08484b538ae69e24de066e08f6c482529d778
Duane Edwards <mail@duaneedwards.net>
2020-07-25 04:23:54 +1000

    Code cleanup: Reduce nesting by returning earlier as per 1n5an1ty's suggestion

--- PatreonClient/Models/IRelationshipExtensions.cs
+++ PatreonClient/Models/IRelationshipExtensions.cs
@@ -8,41 +8,38 @@ namespace PatreonClient.Models
 	{
 		public static IRelationship AssignData<BaseType>(this IRelationship relationship, IReadOnlyCollection<PatreonData> includes, PatreonResponse<BaseType> baseType)
 		{
-			if (baseType?.Data != null)
-			{
-				baseType.Data = includes.FirstOrDefault(x => x.Id == baseType.Data.Id) as PatreonData<BaseType>;
-			}
+			if (baseType?.Data == null) return relationship;
+
+			baseType.Data = includes.FirstOrDefault(x => x.Id == baseType.Data.Id) as PatreonData<BaseType>;
 
 			return relationship;
 		}
 
 		public static IRelationship AssignDataAndRelationship<BaseType, RelationshipType>(this IRelationship relationship, IReadOnlyCollection<PatreonData> includes, PatreonResponse<BaseType, RelationshipType> baseType) where RelationshipType : IRelationship
 		{
-			if (baseType?.Data != null)
-			{
-				baseType.Data = includes.FirstOrDefault(x => x.Id == baseType.Data.Id) as PatreonData<BaseType, RelationshipType>;
+			if (baseType?.Data == null) return relationship;
 
-				baseType.Data?.Relationships?.AssignRelationship(includes);
-			}
+			baseType.Data = includes.FirstOrDefault(x => x.Id == baseType.Data.Id) as PatreonData<BaseType, RelationshipType>;
+
+			baseType.Data?.Relationships?.AssignRelationship(includes);
 
 			return relationship;
 		}
 
 		public static IRelationship AssignCollectionAttributesAndRelationships<BaseType, RelationshipType>(this IRelationship relationship, IReadOnlyCollection<PatreonData> includes, PatreonCollectionResponse<BaseType, RelationshipType> baseCollection) where RelationshipType : IRelationship
 		{
-			if (baseCollection?.Data != null)
-			{
-				foreach (var collectionInstance in baseCollection?.Data)
-				{
-					var include = includes.FirstOrDefault(x => x.Id == collectionInstance.Id) as PatreonData<BaseType, RelationshipType>;
-					if (include != null)
-					{
-						collectionInstance.Attributes = include.Attributes;
-						collectionInstance.Relationships = include.Relationships;
-					}
+			if (baseCollection?.Data == null) return relationship;
 
-					collectionInstance.Relationships?.AssignRelationship(includes);
+			foreach (var collectionInstance in baseCollection.Data)
+			{
+				var include = includes.FirstOrDefault(x => x.Id == collectionInstance.Id) as PatreonData<BaseType, RelationshipType>;
+				if (include != null)
+				{
+					collectionInstance.Attributes = include.Attributes;
+					collectionInstance.Relationships = include.Relationships;
 				}
+
+				collectionInstance.Relationships?.AssignRelationship(includes);
 			}
 
 			return relationship;
