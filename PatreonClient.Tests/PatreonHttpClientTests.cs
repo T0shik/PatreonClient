9fcfb691614dee99e4caa1de60d70068d4a91030
Monty <ma@surgicalcoder.com>
2020-07-26 15:05:42 +0100

    Tests for null / empty / whitespace

---
+++ PatreonClient.Tests/PatreonHttpClientTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+
+namespace PatreonClient.Tests
+{
+    public class PatreonHttpClientTests
+    {
+        [Fact]
+        public void CheckHttpClientIsNotNull ()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new PatreonHttpClient(null, null, "a"));
+
+            Assert.Equal("Value cannot be null. (Parameter 'client')", ex.Message);
+        }
+
+        [Fact]
+        public void CheckAccessToeknIsNotNull()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new PatreonHttpClient(new HttpClient(), null, null));
+
+            Assert.Equal("Value cannot be null or whitespace. (Parameter 'AccessToken')", ex.Message);
+        }
+
+        [Fact]
+        public void CheckAccessToeknIsNotEmpty()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new PatreonHttpClient(new HttpClient(), null, string.Empty));
+
+            Assert.Equal("Value cannot be null or whitespace. (Parameter 'AccessToken')", ex.Message);
+        }
+        [Fact]
+        public void CheckAccessToeknIsNotWhitespace()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new PatreonHttpClient(new HttpClient(), null, " "));
+
+            Assert.Equal("Value cannot be null or whitespace. (Parameter 'AccessToken')", ex.Message);
+        }
+
+    }
+}
